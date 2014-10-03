6527ab0c3c0524f5bbcdfa23301f09e34c29ec1d
Szymon Wanot <szwanot@gmail.com>
2014-10-03 16:43:16 +0200

    downloading service interface

--- DownloadingService/Program.cs
+++ DownloadingService/Program.cs
@@ -3,13 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Topshelf;
 
 namespace DownloadingService
-{
+{   
+    interface IDownloadingService
+	{
+        void Start();
+        void Stop();
+	}
+    
+    class DownloadingService : IDownloadingService
+    {
+
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
-        {        
+        {
+            HostFactory.Run(x => { 
+                
+            });
         }
     }
 }
